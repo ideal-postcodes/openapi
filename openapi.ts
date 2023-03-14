77e4bfd22d0691c6fad997d01431999b5fe07a77
Christopher Blanchard <git@nf3.co>
2023-03-14 11:42:33 +0000

    fix(PAF): Extends Postcode Type

--- openapi.ts
+++ openapi.ts
@@ -600,10 +600,10 @@ export interface components {
      * Postcode Type
      * @description This indicates the type of user. It can only take the values 'S' or 'L' indicating small or large respectively. Large User Postcodes. These are assigned to one single address either due to the large volume of mail received at that address, or because a PO Box or Selectapost service has been set up. Small User Postcodes. These identify a group of Delivery Points.
      *
-     * On average there are 15 Delivery Points per Postcode. However this can vary between 1 and, in some cases, 100. There will never be more than 100 Delivery Points on a Postcode.
+     * On average there are 19 Delivery Points per Postcode. However this can vary between 1 and, in some cases, 100. There will never be more than 100 Delivery Points on a Postcode.
      * @enum {string}
      */
-    paf_postcode_type: "S" | "L";
+    paf_postcode_type: "S" | "L" | "";
     /**
      * Small User Organisation Indicator
      * @description Small User Organisation Indicator can have the values 'Y' or space. A value of 'Y' indicates that a Small User Organisation is present at this address.
